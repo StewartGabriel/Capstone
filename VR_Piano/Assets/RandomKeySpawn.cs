092a1b4430e09cbee592d2a9b127695477e6a9df
Butlchr17 <butlchr17@gmail.com>
2024-11-05 18:11:37 -0800

    Create Random KeySpawn.cs
    Script which is to be placed on an empty game object and which calls the SpawnRandomKey() method from the SpawnKeys class to spawn 8 different keys on the plane with 1 second intervals when the application starts.

---
+++ VR_Piano/Assets/RandomKeySpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomKeySpawn : MonoBehaviour
+{
+    public SpawnKeys spawnKeys;
+
+    private int spawnCount = 8; // Number of times to spawn
+    private float interval = 1.0f; // Interval in seconds between spawns
+
+    private void Start()
+    {
+        // Start the spawn coroutine on start
+        StartCoroutine(SpawnRandomKey());
+    }
+
+    private IEnumerator SpawnRandomKey()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            // Call the SpawnRandomKey method from ObjectSpawner
+            spawnKeys.SpawnRandomKey();
+            
+            // Wait for the specified interval before the next spawn
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
