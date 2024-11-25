23d975a2fe6a2abd8d3c36cc3ccf2c287f17b40b
StewartGabriel <monkeybear626@hotmail.com>
2024-11-24 16:23:20 -0800

    I can now recognize that a MIDI device is being connected and disconnected to the Meta Quest

--- VR_Piano/Assets/Scripts/Plug-ins/PluginInit.cs
+++ VR_Piano/Assets/Scripts/Plug-ins/PluginInit.cs
@@ -10,12 +10,16 @@ public class PluginInit : MonoBehaviour
     AndroidJavaObject _pluginInstance;
 
     PluginInit pluginInit;
+    PlayerInput playerInput;
+    int last_size;
 
     // Start is called before the first frame update
     void Start()
     {
-        pluginInit = GameObject.Find("Canvas").GetComponent<PluginInit>();
+        pluginInit = GameObject.Find("Cube").GetComponent<PluginInit>();
+        playerInput = GameObject.Find("Cube").GetComponent<PlayerInput>();
         InitializePlugIn("com.gabriel.midi.PlugInInstance"); //com.gabriel.miditest.PlugInInstance
+        last_size = 0;
         if (_pluginInstance != null)
         {
             Debug.Log("Instance Created");
@@ -30,51 +34,55 @@ public class PluginInit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Devices Connected: " + _pluginInstance.Call<int>("getDevices"));
+        Debug.Log("Devices Connected: " + _pluginInstance.Call<int>("getDeviceAmount"));
+        if (_pluginInstance.Call<int>("getDeviceAmount") != last_size) //Changes the cube's color if a device is connected or disconnected
+        {
+            last_size = _pluginInstance.Call<int>("getDeviceAmount");
+            playerInput.OnChange();
+            //Create a reciever
+            //_pluginInstance.Call("createReciever");
+        }
     }
 
-    void InitializePlugIn(string pluginName)
+    void InitializePlugIn(string pluginName) //Initialize the plug-in
     {
-        unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
-        _pluginInstance = new AndroidJavaObject(pluginName);
+        unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); //Represents the User as a UnityPlayer
+        unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity"); //Represents the User's activity in the app, the one thing they should be interacting with
+        _pluginInstance = new AndroidJavaObject(pluginName); //Initiallizes the plug-in 
         if (_pluginInstance == null)
         {
             Debug.Log("Plugin Instance Error");
         }
-        _pluginInstance.CallStatic("receiveUnityActivity", unityActivity);
+        _pluginInstance.CallStatic("receiveUnityActivity", unityActivity); // Calls the plug-in
     }
 
-    public void Add()
-    {
-        if (_pluginInstance != null)
-        {
-            var result = _pluginInstance.Call<int>("Add", 5, 6);
-            Debug.Log("Add result from Unity: " + result);
-        }
-        else
-        {
-            Debug.Log("Add Fail");
-        }
-    }
 
-    public void Toast()
-    {
-        if (_pluginInstance != null)
-        {
-            _pluginInstance.Call("Toast", "Hi! from Unity");
-        }
-        else
-        {
-            Debug.Log("Toast Fail");
-        }
-    }
 
-    /*public void CreateDevice()
-    {
 
-    }
+    // TABLET DEBUG METHODS
+    // public void Add() //Test Script to add numbers and log them
+    // {
+    //     if (_pluginInstance != null)
+    //     {
+    //         var result = _pluginInstance.Call<int>("Add", 5, 6);
+    //         Debug.Log("Add result from Unity: " + result);
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Add Fail");
+    //     }
+    // }
 
-    */
+    // public void Toast() //Test Script to show a message to the screen
+    // {
+    //     if (_pluginInstance != null)
+    //     {
+    //         _pluginInstance.Call("Toast", "Hi! from Unity");
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Toast Fail");
+    //     }
+    // }
 
 }
