1d60079038406d00ab4952f4a6038136cffaef16
Butlchr17 <butlchr17@gmail.com>
2025-05-07 11:26:57 -0700

    Added the ability to stop handle movement via an if true statement
    - need to change the if true statement to an if midi message received from the leftmost and rightmost keys

---
+++ VR_Piano/Assets/Scripts/Handle1PositionStorage.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using UnityEngine;
+
+public static class Handle1PositionStorage
+{
+    public static Vector3 savedPosition = Vector3.zero;
+    public static bool hasSavedPosition = false;
+}
